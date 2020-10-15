546b55a2c16ff22b25da562b55bc7f8106121bfd
rmjuarez12 <rmjuarez12@gmail.com>
2020-10-14 22:42:29 -0400

    component/register

--- airbnb-optimal-price/src/components/Register.js
+++ airbnb-optimal-price/src/components/Register.js
@@ -1,9 +1,52 @@
-import React from 'react'
+// Import Dependencies
+import React, { useState } from 'react';
 
 export default function Register() {
+
+  // Declare a variable holding the default empty data
+  const defaultUserData = {name: "", password: "", email: ""};
+
+  // Get the state to hold the form data
+  const [ user, setUser ] = useState(defaultUserData);
+
+  // Function to handle the text field change to set to the user state
+  const handleChange = (e) => {
+    const userData = {...user, [e.target.name]: e.target.value};
+
+    setUser(userData);
+  }
+
+  // Function to handle the form submission
+  const handleSubmission = (e) => {
+    e.preventDefault();
+
+    console.log(user);
+
+    setUser(defaultUserData);
+  }
+  
   return (
-    <div>
-      Register
+    <div className="form-container">
+      <h3>Registration Form</h3>
+
+      <form onSubmit={handleSubmission}>
+      <label htmlFor="name">
+          Name
+          <input type="text" id="name" name="name" value={user.name} onChange={handleChange} />
+        </label>
+
+        <label htmlFor="email">
+          Email
+          <input type="text" id="email" name="email" value={user.email} onChange={handleChange} />
+        </label>
+
+        <label htmlFor="password">
+          Password
+          <input type="password" id="password" name="password" value={user.password} onChange={handleChange} />
+        </label>
+
+        <input type="submit" value="Log in" />
+      </form>
     </div>
   )
 }
