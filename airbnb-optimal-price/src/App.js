68255ce3f050369c6799635a3b41dc8b24e080be
Tony Sorensen <tony48853@gmail.com>
2020-10-14 20:58:51 -0700

    Created utilities folder with axiosWithAuth, listings and user context and PrivateRoute components.

--- airbnb-optimal-price/src/App.js
+++ airbnb-optimal-price/src/App.js
@@ -1,18 +1,19 @@
 // Import Dependencies
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Login from './components/Login';
-import Register from './components/Register';
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+import PrivateRoute from "../utilities/PrivateRoute";
 
 // Import Assets
-import './App.css';
-import Homepage from './components/Homepage';
+import "./App.css";
+import Homepage from "./components/Homepage";
 
 function App() {
   return (
     <Router>
       <div className="App">
-
         <Route exact path="/">
           <Homepage />
         </Route>
@@ -24,7 +25,6 @@ function App() {
         <Route path="/register">
           <Register />
         </Route>
-
       </div>
     </Router>
   );
